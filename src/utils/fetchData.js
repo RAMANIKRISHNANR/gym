92b5f58e6f352fc250afa0e3a9eb2ee0f33d5fc3
RAMANIKRISHNANR <ramanikrishnan.r26@gmail.com>
2023-03-15 15:28:22 +0530

    new

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,7 +3,6 @@ export const exerciseOptions = {
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-     useQueryString: true,
   },
 };
 
